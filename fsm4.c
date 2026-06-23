371720d71f28b0bd67bc344097430f5932bf702c
Alexandre Coelho <93800383+alexandrelesc@users.noreply.github.com>
2026-06-23 12:35:10 -0300

    Refactor ATM state machine and event handlers

--- fsm4.c
+++ fsm4.c
@@ -1,140 +1,118 @@
 #include <stdio.h>
-//Different state of ATM machine
-typedef enum
-{
+
+typedef enum {
     Idle_State,
     Card_Inserted_State,
-    Pin_Eentered_State,
+    Pin_Entered_State,
     Option_Selected_State,
-    Amount_Entered_State,
+    Amount_Entered_State
 } eSystemState;
-//Different type events
-typedef enum
-{
+
+typedef enum {
     Card_Insert_Event,
     Pin_Enter_Event,
     Option_Selection_Event,
     Amount_Enter_Event,
-    Amount_Dispatch_Event
+    Amount_Dispatch_Event,
+    Invalid_Event
 } eSystemEvent;
-//Prototype of eventhandlers
-eSystemState AmountDispatchHandler(void)
-{
-    printf("AmountDispatchHandler\r\n");
+
+eSystemState AmountDispatchHandler(void) {
+    printf("Dinheiro entregue\n");
     return Idle_State;
 }
-eSystemState EnterAmountHandler(void)
-{
-    printf("EnterAmountHandler \r\n");
+
+eSystemState EnterAmountHandler(void) {
+    printf("Valor informado\n");
     return Amount_Entered_State;
 }
-eSystemState OptionSelectionHandler(void)
-{
-    printf("OptionSelectionHandler \r\n");
+
+eSystemState OptionSelectionHandler(void) {
+    printf("Opcao selecionada\n");
     return Option_Selected_State;
 }
-eSystemState EnterPinHandler(void)
-{
-    printf("EnterPinHandler \r\n");
-    return Pin_Eentered_State;
+
+eSystemState EnterPinHandler(void) {
+    printf("PIN informado\n");
+    return Pin_Entered_State;
 }
-eSystemState InsertCardHandler(void)
-{
-    printf("InsertCardHandler \r\n");
+
+eSystemState InsertCardHandler(void) {
+    printf("Cartao inserido\n");
     return Card_Inserted_State;
 }
 
-eSystemEvent ReadEvent(void)
-{
-  char inputEvent = 0;
-  printf("waiting for a new event: ");
-  scanf("%c", &inputEvent);
-  printf("%c\r\n", inputEvent);
-  
-  if(inputEvent == 'a'){
-    return Card_Insert_Event;
-  }
+eSystemEvent ReadEvent(void) {
+    char inputEvent;
 
-  if(inputEvent == 'b'){
-    return Pin_Enter_Event;
-  }
+    printf("\nEventos:\n");
+    printf("a - inserir cartao\n");
+    printf("b - digitar PIN\n");
+    printf("c - selecionar opcao\n");
+    printf("d - informar valor\n");
+    printf("e - entregar dinheiro\n");
+    printf("Escolha: ");
 
-  if(inputEvent == 'c'){
-    return Option_Selection_Event;
-  }
+    scanf(" %c", &inputEvent);
 
-  if(inputEvent == 'd'){
-    return Amount_Enter_Event;
-  }
-  
-  if(inputEvent == 'e'){
-    return Amount_Dispatch_Event;
-  }
+    if (inputEvent == 'a') return Card_Insert_Event;
+    if (inputEvent == 'b') return Pin_Enter_Event;
+    if (inputEvent == 'c') return Option_Selection_Event;
+    if (inputEvent == 'd') return Amount_Enter_Event;
+    if (inputEvent == 'e') return Amount_Dispatch_Event;
+
+    return Invalid_Event;
 }
 
-int main(int argc, char *argv[])
-{
+int main(void) {
     eSystemState eNextState = Idle_State;
     eSystemEvent eNewEvent;
-    while(1)
-    {
-        //Read system Events
-        eSystemEvent eNewEvent = ReadEvent();
-        switch(eNextState)
-        {
-        case Idle_State:
-        {
-            printf("Idle_State\r\n");
-            if(Card_Insert_Event == eNewEvent)
-            {
-                printf("Card_Insert_Event\r\n");
-                eNextState = InsertCardHandler();
-            }
-        }
-        break;
-        case Card_Inserted_State:
-        {
-            printf("Card_Inserted_State\r\n");
-            if(Pin_Enter_Event == eNewEvent)
-            {
-                printf("Pin_Enter_Event\r\n");
-                eNextState = EnterPinHandler();
-            }
-        }
-        break;
-        case Pin_Eentered_State:
-        {
-            printf("Pin_Eentered_State\r\n");
-            if(Option_Selection_Event == eNewEvent)
-            {
-                printf("Option_Selection_Event\r\n");
-                eNextState = OptionSelectionHandler();
-            }
-        }
-        break;
-        case Option_Selected_State:
-        {
-            printf("Option_Selected_State\r\n");
-            if(Amount_Enter_Event == eNewEvent)
-            {
-                printf("EnterAmountHandler\r\n");
-                eNextState = EnterAmountHandler();
-            }
-        }
-        break;
-        case Amount_Entered_State:
-        {
-            printf("EnterAmountHandler\r\n");
-            if(Amount_Dispatch_Event == eNewEvent)
-            {
-                printf("AmountDispatchHandler\r\n");
-                eNextState = AmountDispatchHandler();
-            }
-        }
-        break;
-        default:
-            break;
+
+    while (1) {
+        eNewEvent = ReadEvent();
+
+        switch (eNextState) {
+            case Idle_State:
+                printf("Estado atual: Idle_State\n");
+                if (eNewEvent == Card_Insert_Event)
+                    eNextState = InsertCardHandler();
+                else
+                    printf("Evento invalido nesse estado\n");
+                break;
+
+            case Card_Inserted_State:
+                printf("Estado atual: Card_Inserted_State\n");
+                if (eNewEvent == Pin_Enter_Event)
+                    eNextState = EnterPinHandler();
+                else
+                    printf("Evento invalido nesse estado\n");
+                break;
+
+            case Pin_Entered_State:
+                printf("Estado atual: Pin_Entered_State\n");
+                if (eNewEvent == Option_Selection_Event)
+                    eNextState = OptionSelectionHandler();
+                else
+                    printf("Evento invalido nesse estado\n");
+                break;
+
+            case Option_Selected_State:
+                printf("Estado atual: Option_Selected_State\n");
+                if (eNewEvent == Amount_Enter_Event)
+                    eNextState = EnterAmountHandler();
+                else
+                    printf("Evento invalido nesse estado\n");
+                break;
+
+            case Amount_Entered_State:
+                printf("Estado atual: Amount_Entered_State\n");
+                if (eNewEvent == Amount_Dispatch_Event)
+                    eNextState = AmountDispatchHandler();
+                else
+                    printf("Evento invalido nesse estado\n");
+                break;
         }
     }
+
     return 0;
 }
